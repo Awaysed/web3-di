5843da4b4dfb506bd99d5811dced411375430f73
Awaysed <122900171@qq.com>
2024-09-26 12:58:01 +0800

    updaete:vant

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { Button } from 'vant';
+import Vant from 'vant';
 import App from './App.vue'
 import router from './router'
-
+import 'vant/lib/index.css';
 const app = createApp(App)
-
+app.use(Vant)
 app.use(createPinia())
 app.use(router)
+app.use(Button);
 
 app.mount('#app')

2a48ddf1c02e22df1234a185c765713b490f7cf3
Awaysed <122900171@qq.com>
2024-10-31 15:39:53 +0800

    update:change

--- src/components/web3-api.vue
+++ src/components/web3-api.vue
@@ -9,11 +9,13 @@ var web3 = new Web3(
   Web3.givenProvider || 'wss://sepolia.infura.io/ws/v3/9592f660765f4dd19d74b5248ba9b66c'
 )
 const account = web3.eth.accounts.create('123')
-console.log(account)
+// console.log(account)
 const mount = ref(null)
 web3.eth.getBalance(account.address).then((res) => {
   mount.value = res
 })
+const num1 = web3.utils.toWei('0.3', 'tether')
+console.log(1111, num1)
 </script>
 
 <style scoped lang="less"></style>

11b7c469b3abec6d3b95d5e29545a31f06e7786c
Awaysed <122900171@qq.com>
2024-11-19 19:46:36 +0800

    update:12

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,7 @@ import Vant from 'vant'
 import App from './App.vue'
 import router from './router'
 import 'vant/lib/index.css'
-import * as buffer from 'buffer' //引入buffer
 
-if (typeof (window as any).Buffer === 'undefined') {
-  // 判断当前环境是否有Buffer对象
-  ;(window as any).Buffer = buffer.Buffer // Buffer对象不存在则创建导入的buffer
-}
 const app = createApp(App)
 app.use(Vant)
 app.use(createPinia())
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import nodePolyfills from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), nodePolyfills()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
